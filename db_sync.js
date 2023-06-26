9a9e0a802014c78f3323fa23e90afeab08ee2235
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2023-06-25 19:49:34 -0600

    Update db_sync.js

--- db_sync.js
+++ db_sync.js
@@ -1,118 +1,100 @@
 const fs = require('fs');
 const mysql = require('mysql');
 
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: '10.0.0.52',
+// Create a connection pool to the database
+const pool = mysql.createPool({
+  host: 'localhost',
   user: 'web',
   password: 'web',
   database: 'web',
 });
 
-// Connect to the database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
-  }
+// Read the existing users data from the users.json file
+const existingUsersData = fs.readFileSync('./users.json', 'utf8');
+let existingUsers = JSON.parse(existingUsersData);
 
-  console.log('Connected to the database.');
-
-  // Read the existing users data from the users.json file
-  const existingUsersData = fs.readFileSync('./users.json', 'utf8');
-  const existingUsers = JSON.parse(existingUsersData);
-
-  // Map the existing users data to an object for easy lookup
-  const existingUsersMap = {};
-  existingUsers.forEach((user) => {
-    existingUsersMap[user.id] = user;
+// Function to fetch user data from the database
+function fetchUserData() {
+  return new Promise((resolve, reject) => {
+    pool.query('SELECT * FROM accounts', (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
   });
+}
 
-  // Execute the query to fetch user data
-  connection.query('SELECT * FROM accounts', (err, results) => {
-    if (err) {
-      console.error('Error retrieving user data:', err);
-      connection.end();
-      return;
-    }
+// Function to synchronize the database with the JSON file
+async function syncDatabaseWithJSON() {
+  try {
+    // Fetch user data from the database
+    const dbUsers = await fetchUserData();
 
-    // Synchronize database data with the existing users
-    const synchronizedUsers = results.map((row) => {
-      const { id, username, password, email } = row;
-      const existingUser = existingUsersMap[id];
-
-      // Fetch bounties for the user
-      const bounties = existingUser ? existingUser.bounties : []; // Use existing bounties or fetch from the database
-
-      // Fetch leaderboard position for the user
-      const leaderboardPosition = existingUser ? existingUser.leaderboardPosition : null; // Use existing position or fetch from the database
-
-      // Fetch missions for the user
-      const missions = existingUser ? existingUser.missions : []; // Use existing missions or fetch from the database
-
-      // Fetch NFTs for the user
-      const nfts = existingUser ? existingUser.nfts : []; // Use existing NFTs or fetch from the database
-
-      return { id, username, password, email, bounties, leaderboardPosition, missions, nfts };
+    // Convert the fetched user data to an object for easy lookup
+    const dbUsersMap = {};
+    dbUsers.forEach((user) => {
+      dbUsersMap[user.id] = user;
     });
 
-    // Write the synchronized users data to the users.json file
-    fs.writeFileSync('./users.json', JSON.stringify(synchronizedUsers));
+    // Iterate over the existing users and synchronize with the database
+    existingUsers = existingUsers.map((user) => {
+      const dbUser = dbUsersMap[user.id];
 
-    console.log('Users data synchronized successfully.');
-
-    // Close the database connection
-    connection.end();
-  });
-});
-
-// Watch for changes in the users.json file
-// Watch for changes in the users.json file
-fs.watch('./users.json', (eventType) => {
-    if (eventType === 'change') {
-      // Read the updated users data from the users.json file
-      const updatedUsersData = fs.readFileSync('./users.json', 'utf8');
-      const updatedUsers = JSON.parse(updatedUsersData);
-  
-      // Loop through the updated users and update the corresponding database records
-      updatedUsers.forEach((user) => {
+      if (dbUser) {
+        // User exists in the database, update the user
         const { id, username, password, email, bounties, leaderboardPosition, missions, nfts } = user;
-  
-        // Update the database records with the updated user data
-        const query = `
-          UPDATE accounts
-          SET username = '${username}', password = '${password}', email = '${email}'
-          WHERE id = ${id}`;
-        connection.query(query, (err, results) => {
+        const query = `UPDATE accounts SET username = '${username}', password = '${password}', email = '${email}', bounties = '${JSON.stringify(bounties)}', leaderboardPosition = ${leaderboardPosition}, missions = '${JSON.stringify(missions)}', nfts = '${JSON.stringify(nfts)}' WHERE id = ${id}`;
+
+        pool.query(query, (err) => {
           if (err) {
             console.error(`Error updating user ${id} in the database:`, err);
           }
         });
-      });
-  
-      console.log('Database records updated with the changes from users.json.');
-  
-      // Fetch the latest data from the database after updating the records
-      connection.query('SELECT * FROM accounts', (err, results) => {
-        if (err) {
-          console.error('Error retrieving user data:', err);
-          return;
-        }
-  
-        // Map the fetched data to the existingUsers object
-        const existingUsers = results.map((row) => {
-          const { id, username, password, email } = row;
-          return { id, username, password, email };
+
+        return user; // Return the existing user
+      } else {
+        // User doesn't exist in the database, insert the user
+        const { id, username, password, email, bounties, leaderboardPosition, missions, nfts } = user;
+        const query = `INSERT INTO accounts (id, username, password, email, bounties, leaderboardPosition, missions, nfts) VALUES (${id}, '${username}', '${password}', '${email}', '${JSON.stringify(bounties)}', ${leaderboardPosition}, '${JSON.stringify(missions)}', '${JSON.stringify(nfts)}')`;
+
+        pool.query(query, (err) => {
+          if (err) {
+            console.error(`Error inserting user ${id} into the database:`, err);
+          }
         });
-  
-        // Write the updated user data back to the users.json file
-        fs.writeFileSync('./users.json', JSON.stringify(existingUsers));
-  
-        console.log('Users data synchronized successfully.');
-      });
+
+        return user; // Return the existing user
+      }
+    });
+
+    console.log('Database synchronized with the JSON file.');
+  } catch (err) {
+    console.error('Error retrieving user data from the database:', err);
+  }
+}
+
+// Function to synchronize the JSON file with the database
+function syncJSONWithDatabase() {
+  fs.writeFile('./users.json', JSON.stringify(existingUsers), (err) => {
+    if (err) {
+      console.error('Error writing to users.json:', err);
+    } else {
+      console.log('JSON file synchronized with the database.');
     }
   });
-  
-    console.log('Database records updated with the changes from users.json.');
+}
+
+// Initial synchronization
+syncDatabaseWithJSON();
+
+// Watch for changes in the users.json file
+fs.watch('./users.json', (eventType) => {
+  if (eventType === 'change') {
+    const updatedUsersData = fs.readFileSync('./users.json', 'utf8');
+    existingUsers = JSON.parse(updatedUsersData);
+
+    syncDatabaseWithJSON();
   }
 });
